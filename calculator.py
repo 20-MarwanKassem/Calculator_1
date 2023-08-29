8754b9b7cbda0b8f9b8893c08822d93716916edf
Marwan Kassem <136387695+20-MarwanKassem@users.noreply.github.com>
2023-08-29 17:06:46 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,50 @@
+def addition(x, y):
+    return x + y
+
+def subtraction(x, y):
+    return x - y
+
+def multiplication(x, y):
+    return x * y
+
+def division(x, y):
+    return x / y
+
+
+print("Select operation.")
+print("1. Add")
+print("2. Subtract")
+print("3. Multiply")
+print("4. Divide")
+
+while True:
+    choice = input("Enter choice (1/2/3/4): ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter the first number: "))
+            num2 = float(input("Enter the second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+        if choice == '1':
+            print(num1, "+", num2, "=", addition(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtraction(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiplication(num1, num2))
+
+        elif choice == '4':
+            if num2 != 0:
+                print(num1, "/", num2, "=", division(num1, num2))
+            else:
+                print("Error: Cannot divide by zero!")
+                
+        next_calculation = input("Perform another calculation? (yes/no): ")
+        if next_calculation == "no":
+            break
+    else:
+        print("Invalid input.")
